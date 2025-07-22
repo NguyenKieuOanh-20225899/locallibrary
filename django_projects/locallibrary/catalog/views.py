1cedbd1fee04582e483fb284a526ecba14dc0984
NguyenKieuOanh-20225899 <nguyenkieuoanh17720041772004@gmail.com>
2025-07-22 10:44:42 +0700

    part5:Creating our home page

--- django_projects/locallibrary/catalog/views.py
+++ django_projects/locallibrary/catalog/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
+from catalog.models import Book, Author, BookInstance, Genre
 
-# Create your views here.
-from django.http import HttpResponse
+
 
 def index(request):
-    return HttpResponse("Welcome to the catalog home page.")
+    """View function for home page of site."""
+
+    # Đếm số lượng các đối tượng chính
+    num_books = Book.objects.count()
+    num_instances = BookInstance.objects.count()
+
+    # Đếm book instance có status = 'a' (available)
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # Đếm số tác giả
+    num_authors = Author.objects.count()
+
+    # Truyền dữ liệu vào template
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    # Render trang index.html với dữ liệu context
+    return render(request, 'catalog/index.html', context)
