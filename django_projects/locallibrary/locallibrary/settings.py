0fd5abb72590086ab6b5abfb533aa9048ce33f96
NguyenKieuOanh-20225899 <nguyenkieuoanh17720041772004@gmail.com>
2025-07-23 18:54:40 +0700

    Update settings.py

--- django_projects/locallibrary/locallibrary/settings.py
+++ django_projects/locallibrary/locallibrary/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'locallibrary.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydb',
+        'USER': 'root',
+        'PASSWORD': '177165@Oanh',
+        'HOST': 'localhost',   # hoặc IP MySQL server
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
