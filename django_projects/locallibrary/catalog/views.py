ca871f1d5fa4cf32fbf7153a99bbfa8ba63ca2d8
NguyenKieuOanh-20225899 <nguyenkieuoanh17720041772004@gmail.com>
2025-07-23 07:50:32 +0700

    part7:Sessions framework

--- django_projects/locallibrary/catalog/views.py
+++ django_projects/locallibrary/catalog/views.py
@@ -15,13 +15,15 @@ def index(request):
 
     # Đếm số tác giả
     num_authors = Author.objects.count()
-
+    num_visits = request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits + 1
     # Truyền dữ liệu vào template
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_visits': num_visits,
     }
 
     # Render trang index.html với dữ liệu context
