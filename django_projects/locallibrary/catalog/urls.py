1856479db94eb5cb83dfc7c8a8f19914c06e1588
NguyenKieuOanh-20225899 <nguyenkieuoanh17720041772004@gmail.com>
2025-07-25 10:32:59 +0700

    part9: Working with forms

--- django_projects/locallibrary/catalog/urls.py
+++ django_projects/locallibrary/catalog/urls.py
@@ -8,6 +8,16 @@ urlpatterns = [
     path('books/', views.BookListView.as_view(), name='books'),
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'), 
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+    path('borrowed/', views.LoanedBooksAllListView.as_view(), name='all-borrowed'),
+    path('book/<uuid:pk>/return/', views.return_book, name='book-return'),
+    path('author/create/', views.AuthorCreate.as_view(), name='author-create'),
+    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author-update'),
+    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author-delete'),
+    path('author/<int:pk>/', views.AuthorDetail.as_view(), name='author-detail'),
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
+
+
 ]
 
 
--- django_projects/locallibrary/catalog/views.py
+++ django_projects/locallibrary/catalog/views.py
@@ -5,7 +5,16 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import View
 from django.contrib.auth.decorators import permission_required
 
+import datetime
 
+from django.contrib.auth.decorators import login_required, permission_required
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.shortcuts import render
+
+from catalog.forms import RenewBookForm
+from django.contrib.auth.mixins import PermissionRequiredMixin
 def index(request):
     """View function for home page of site."""
 
@@ -70,3 +79,69 @@ class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
         return BookInstance.objects.filter(
             borrower=self.request.user
         ).filter(status__exact='o').order_by('due_back')
+@login_required
+@permission_required('catalog.can_mark_returned', raise_exception=True)
+def return_book(request, pk):
+    """View function to return a borrowed book (by changing status to available)."""
+    book_instance = get_object_or_404(BookInstance, pk=pk)
+    book_instance.status = 'a'  # Mark as available
+    book_instance.borrower = None
+    book_instance.due_back = None
+    book_instance.save()
+    return HttpResponseRedirect(reverse('my-borrowed'))
+def renew_book_librarian(request, pk):
+    """View function for renewing a specific BookInstance by librarian."""
+    book_instance = get_object_or_404(BookInstance, pk=pk)
+
+    if request.method == 'POST':
+        form = RenewBookForm(request.POST)
+        if form.is_valid():
+            # Ghi ngày mới vào trường due_back
+            book_instance.due_back = form.cleaned_data['renewal_date']
+            book_instance.save()
+            return HttpResponseRedirect(reverse('all-borrowed'))
+
+    else:
+        # Nếu GET: hiển thị form với ngày mặc định là 3 tuần từ hôm nay
+        proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
+        form = RenewBookForm(initial={'renewal_date': proposed_renewal_date})
+
+    context = {
+        'form': form,
+        'book_instance': book_instance,
+    }
+
+    return render(request, 'catalog/book_renew_librarian.html', context)
+
+
+class LoanedBooksAllListView(PermissionRequiredMixin, generic.ListView):
+    model = BookInstance
+    permission_required = 'catalog.can_mark_returned'
+    template_name = 'catalog/bookinstance_list_borrowed_all.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(status__exact='o').order_by('due_back')
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from catalog.models import Author
+
+class AuthorCreate(CreateView):
+    model = Author
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+    initial = {'date_of_death': '11/06/2020'}
+
+class AuthorUpdate(UpdateView):
+    model = Author
+    fields = '__all__'  
+class AuthorDelete(DeleteView):
+    model = Author
+    success_url = reverse_lazy('authors')  
+from django.views.generic import DetailView
+
+class AuthorDetail(DetailView):
+    model = Author
+
+class AuthorListView(generic.ListView):
+    model = Author
+    template_name = 'catalog/author_list.html'
